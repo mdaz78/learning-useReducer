7dde54f9b1d55e1831339edeaa7ef59caae40b4c
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-27 09:07:29 +0530

    refactor using useReducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,32 +1,81 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+interface State {
+  count: number;
+  valueToAdd: number;
+}
+
+interface Action {
+  type: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  payload?: any;
+}
+
+const actions = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  ADD_VALUE: "ADD_VALUE",
+  SET_VALUE_TO_ADD: "SET_VALUE_TO_ADD",
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case actions.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case actions.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case actions.SET_VALUE_TO_ADD:
+      return { ...state, valueToAdd: action.payload };
+    case actions.ADD_VALUE:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+    default:
+      return state;
+  }
+};
 
 const Counter = ({ initialCount }: { initialCount: number }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  console.log(state);
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({
+      type: actions.INCREMENT,
+    });
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({
+      type: actions.DECREMENT,
+    });
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(e.target.value) || 0;
-    setValueToAdd(value);
+    dispatch({
+      type: actions.SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    dispatch({
+      type: actions.ADD_VALUE,
+    });
   };
 
   return (
     <div>
       <div>
-        <h1>Count is {count}</h1>
+        <h1>Count is {state.count}</h1>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
       </div>
@@ -40,7 +89,7 @@ const Counter = ({ initialCount }: { initialCount: number }) => {
             <input
               type="number"
               id="add-value"
-              value={valueToAdd || ""}
+              value={state.valueToAdd || ""}
               onChange={handleChange}
             />
             <button>Add a lot</button>
