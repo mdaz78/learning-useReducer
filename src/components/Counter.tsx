a6ffcfeab7673df745d08dfa0e2597887757e881
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-27 08:15:48 +0530

    update the counter with a 'add a lot' handler

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Counter = ({ initialCount }: { initialCount: number }) => {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -11,11 +12,40 @@ const Counter = ({ initialCount }: { initialCount: number }) => {
     setCount(count - 1);
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setCount(count + valueToAdd);
+  };
+
   return (
     <div>
-      <h1>Count is {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <div>
+        <h1>Count is {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+
+      <br />
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="add-value">Add a lot</label>
+            <input
+              type="number"
+              id="add-value"
+              value={valueToAdd || ""}
+              onChange={handleChange}
+            />
+            <button>Add a lot</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
